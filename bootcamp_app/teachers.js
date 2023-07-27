cb644d2b90192b8f423a69a2a4b3aabb6440f8e1
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-27 18:55:39 +0000

    SQL injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-const cohort = process.argv[2];
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,17 +6,29 @@ const pool = new Pool({
   database: 'vagrant'
 });
 
-pool.query(`
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+  `;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+
+
+
